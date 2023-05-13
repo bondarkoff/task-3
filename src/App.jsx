586dcff32d42416f5089f84805cbbd03a681cb14
bondarkoff <yehor.bondarkoff@gmail.com>
2023-05-13 21:01:41 +0300

    removed strict mode && yarn add uuid && displayed all items on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,50 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+    const [country, setCountry] = useState('');
+    const [universities, setUniversities] = useState([]);
+
     useEffect(() => {
         async function getUniversities() {
-            const itemsResponse = await axios.get('http://universities.hipolabs.com/search');
+            const itemsResponse = await axios.get(
+                `http://universities.hipolabs.com/search?country=${country}`,
+            );
+            setUniversities(itemsResponse.data);
             console.log(itemsResponse.data);
         }
         getUniversities();
     }, []);
 
+    const handleSearch = async () => {
+        try {
+            const itemsResponse = await axios.get(
+                `http://universities.hipolabs.com/search?country=${country}`,
+            );
+            setUniversities(itemsResponse.data);
+        } catch (error) {
+            alert('Error while searching for universities');
+            console.log(error);
+        }
+    };
+
     return (
         <div className='container'>
             <h1>University Database</h1>
-            <input type='text' placeholder='Enter university name' />
+            <input
+                type='text'
+                placeholder='Enter university name'
+                value={country}
+                onChange={e => setCountry(e.target.value)}
+            />
+            <button onClick={handleSearch}>Search</button>
             <button>Reset</button>
+            <div className=''>
+                {universities.map(university => (
+                    <li key={uuidv4()}>{university.name}</li>
+                ))}
+            </div>
         </div>
     );
 }
