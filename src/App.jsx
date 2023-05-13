1ea23f1481f4a0a2f37195f50c0dfd6c7cb9d08a
bondarkoff <yehor.bondarkoff@gmail.com>
2023-05-13 20:19:19 +0300

    clean vite+react project && yarn add sass

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className='container'>
+            <h1>University Database</h1>
+            <input type='text' placeholder='Enter university name' />
+            <button>Reset</button>
+        </div>
+    );
+}
+
+export default App;
