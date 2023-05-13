de2a7727319e69783e73ff60d3be7f78464b9f1e
bondarkoff <yehor.bondarkoff@gmail.com>
2023-05-13 20:33:09 +0300

    yarn add axios && axios.get university list

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
 function App() {
+    useEffect(() => {
+        async function getUniversities() {
+            const itemsResponse = await axios.get('http://universities.hipolabs.com/search');
+            console.log(itemsResponse.data);
+        }
+        getUniversities();
+    }, []);
+
     return (
         <div className='container'>
             <h1>University Database</h1>
